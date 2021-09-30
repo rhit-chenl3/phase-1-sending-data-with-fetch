7845459153d1ec8cf008019ba15eacee7b5e3957
Leo Chen <chenl3@rose-hulman.edu>
2021-09-30 17:32:17 -0400

    commit tree 85d61f799948cab1056f8b3c29495559f93a4b6b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const url = 'http://localhost:3000/users'; 
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    return fetch(url, configurationObject)
+        .then(resp => resp.json())
+        .then(results => {
+            const newP = document.createElement('p');
+            newP.textContent = results.id;
+            document.querySelector('body').append(newP);
+        }).catch(error => {
+            console.error(error);
+            console.error('Attempted URL:', url);
+            console.error('Attempted Options:', configurationObject);
+            const newP = document.createElement('p');
+            newP.textContent = error;
+            document.querySelector('body').append(newP);
+        });
+}
